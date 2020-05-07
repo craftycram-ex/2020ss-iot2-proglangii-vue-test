7e099617b77a539d3adfd2a396b95680b9975576
craftycram <github@craftycram.net>
2020-05-07 10:17:43 +0200

    removed unnecessary code in Plan.vue

--- src/views/Plan.vue
+++ src/views/Plan.vue
@@ -2,26 +2,7 @@
   <div>
     <h1>{{msg}}</h1>
     <span>Wochentag</span>
-  <select name="Wochentag" id="selectBox">
-          <option>Montag</option>
-          <option>Dienstag</option>
-          <option>Mittwoch</option>
-          <option>Donnerstag</option>
-          <option>Freitag</option>
-        </select>
-    
-        <br>
-        <table>
-          <tr>Kategorie</tr>
-          <tr>Infos</tr>
-          <tr>Kosten</tr>
-          <tr>Tag</tr>
-          <tr>Downvotes</tr>
-          <tr>ID</tr>
-          <tr>Labels</tr>
-          <tr>Name</tr>
-          <tr>Upvotes</tr>
-        </table>
+    <!-- Add components here -->
   </div>
 </template>
 

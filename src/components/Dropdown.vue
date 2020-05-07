cfcb0d666016f0c405ee3f0feaffde3a5a318a34
craftycram <github@craftycram.net>
2020-05-07 10:14:51 +0200

    added components

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <select name="Wochentag" id="selectBox">
+          <option>Montag</option>
+          <option>Dienstag</option>
+          <option>Mittwoch</option>
+          <option>Donnerstag</option>
+          <option>Freitag</option>
+        </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+        <table>
+          <tr>Kategorie</tr>
+          <tr>Infos</tr>
+          <tr>Kosten</tr>
+          <tr>Tag</tr>
+          <tr>Downvotes</tr>
+          <tr>ID</tr>
+          <tr>Labels</tr>
+          <tr>Name</tr>
+          <tr>Upvotes</tr>
+        </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
